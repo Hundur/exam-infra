297a49cab279653cea605d565225a9b5c0d5dc37
Jesper <jesper.dahl.ellingsen@hotmail.com>
2018-12-01 19:18:54 +0100

    Getting tired of this

--- terraform/graphite.tf
+++ terraform/graphite.tf
@@ -1,5 +1,5 @@
 # Create a hosted graphite, and configure the app to use it
 resource "heroku_addon" "hostedgraphite" {
-  app  = "${heroku_app.production.name}"
+  app  = "${heroku_app.ci.name}"
   plan = "hostedgraphite"
 }
