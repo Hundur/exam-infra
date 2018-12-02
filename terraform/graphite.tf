c28445f41f9cb8749741ddb214148ed6dbad2590
Jesper <jesper.dahl.ellingsen@hotmail.com>
2018-12-02 18:13:43 +0100

    Changing up graphitenames

--- terraform/graphite.tf
+++ terraform/graphite.tf
@@ -1,15 +1,15 @@
 # Create a hosted graphite, and configure the app to use it
-resource "heroku_addon" "hostedgraphite" {
+resource "heroku_addon" "hostedgraphite_ci" {
   app  = "${heroku_app.ci.name}"
   plan = "hostedgraphite"
 }
 
-resource "heroku_addon" "hostedgraphite" {
+resource "heroku_addon" "hostedgraphite_stage" {
   app  = "${heroku_app.staging.name}"
   plan = "hostedgraphite"
 }
 
-resource "heroku_addon" "hostedgraphite" {
+resource "heroku_addon" "hostedgraphite_production" {
   app  = "${heroku_app.production.name}"
   plan = "hostedgraphite"
 }
